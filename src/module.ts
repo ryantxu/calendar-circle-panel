30014139e4ebc0fe28dea89917de1225c36c4aa8
Ryan McKinley <ryantxu@gmail.com>
2019-11-14 16:03:11 -0800

    copy from template

---
+++ src/module.ts
@@ -0,0 +1,6 @@
+import { PanelPlugin } from '@grafana/data';
+import { SimpleOptions, defaults } from './types';
+import { CircularCalendarPanel } from './CircularCalendarPanel';
+import { CircularCalendarEditor } from './CircularCalendarEditor';
+
+export const plugin = new PanelPlugin<SimpleOptions>(CircularCalendarPanel).setDefaults(defaults).setEditor(CircularCalendarEditor);
